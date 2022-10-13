c017ba8076ddd9be09ec8f9bffa3748fa91bd66f
Elke Boonen <u0116591@thomasmore.be>
2022-10-13 14:02:00 +0200

    13/10

---
+++ Ranking - IMS/LR.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ranking___IMS
+{
+    class LR
+    {
+        public double m { get; private set; }
+        public double b { get; private set; }
+        public double R2 { get; private set; }
+
+        public LR(double[] x, double[] y)
+        {
+            this.Calculate(x,y);
+        }
+        private void Calculate(double[] x, double[] y)
+        { 
+            int n = x.Length;
+
+            double sum_x = x.Sum();
+            double sum_y = y.Sum();
+
+            double sum_xy = 0;
+            double sum_x2 = 0;
+            double sum_y2 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum_xy += x[i] * y[i];
+                sum_x2 += x[i] * x[i]; //Math.Pow()
+                sum_y2 += y[i] * y[i]; //Math.Pow()
+
+            }
+
+            m = ((n * sum_xy) - (sum_x * sum_y)) / (n * sum_x2 - Math.Pow(sum_x, 2));
+            m = Math.Round(m, 4);
+            b = (sum_y-(m* sum_x))/n;
+            b = Math.Round(b, 4);
+
+            R2 = Math.Pow(((n * sum_xy) - (sum_x * sum_y)) / (Math.Sqrt(n * sum_x2 - Math.Pow(sum_x, 2)) * Math.Sqrt(n * sum_y2 - Math.Pow(sum_y, 2))), 2);
+
+        }
+
+        public double Predict(double x)
+        {
+            return m * x + b;
+        }
+    }
+}
---
+++ Ranking - IMS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ranking___IMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            LR chimp = new LR(new double[] {1,2,3,4,5,6,7,8 }, new double[] {30,45,51,57,60,65,70,71 });
+            Console.WriteLine(chimp.m + " " + chimp.b);
+            Console.WriteLine(chimp.Predict(4));
+            Console.WriteLine(chimp.R2);
+
+            List<double> x = new List<double>();
+            List<double> y = new List<double>();
+
+
+            foreach (var item in File.ReadAllLines("wine.csv"))
+            {
+                string[] items = item.Split(',');
+                try
+                {
+                    x.Add(Convert.ToDouble(items[10]));
+                    y.Add(Convert.ToDouble(items[11]));
+                }
+                catch { }
+
+            }
+
+            LR wine = new LR(x.ToArray(), y.ToArray());
+            Console.WriteLine(wine.m + " " + chimp.b);
+            Console.WriteLine(wine.Predict(12));
+            Console.WriteLine(wine.R2);
+        }
+    }
+}
