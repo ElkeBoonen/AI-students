0718d5334fd6a52425bdf93ee1c422c3ab97c8dd
Elke Boonen <u0116591@thomasmore.be>
2022-10-18 12:40:32 +0200

    IMS 18/10

---
+++ N QUEENS - IMS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace N_QUEENS___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NQueens queens = new NQueens(10);
+            queens.Solve();
+
+        }
+    }
+}
