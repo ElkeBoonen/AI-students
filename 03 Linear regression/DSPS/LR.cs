ff7acf6865bbd3342715a7ba34a33e1590a28d9d
Elke Boonen <u0116591@thomasmore.be>
2023-10-05 15:10:09 +0200

    DSPS 5/10

---
+++ 03 Linear regression/DSPS/LR.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPS
+{
+    class LR
+    {
+
+        //y=mx+b --> m = slope, b = intercept
+        private int[] x { get; set; }
+        private int[] y { get; set; }
+        public LR(int[] xvalues, int[] yvalues)
+        {
+            x = xvalues;
+            y = yvalues;
+        }
+
+        public double Slope() //n∑xy−∑x∑y / n∑x^2-(∑x)^2 ​
+        {
+            int xtotals = x.Sum();
+            int ytotals = y.Sum();
+            int n = x.Length;
+
+            int xytotals = 0;
+            int x2totals = 0;
+            for (int i = 0; i < n; i++)
+            {
+                xytotals += x[i] * y[i];
+                x2totals += x[i] * x[i];
+            }
+
+            double up = n * xytotals - xtotals * ytotals;
+            double down = n * x2totals - xtotals*xtotals;
+            return Math.Round(up / down,4);
+            
+        }
+
+        public double Intercept() //∑y−m∑x / n
+        {
+            return Math.Round((y.Sum() - Slope() * x.Sum()) / x.Length,4);
+        }
+
+        public double Predict(int x)
+        { 
+            return Math.Round(Slope()*x + Intercept(),4);
+        }
+    }
+}
---
+++ 03 Linear regression/DSPS/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LR chimps = new LR(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }
+                             , new int[] { 30, 45, 51, 57, 60, 65, 70, 71});
+            Console.WriteLine(chimps.Slope());
+            Console.WriteLine(chimps.Intercept());
+            Console.WriteLine(chimps.Predict(4));
+        }
+    }
+}
