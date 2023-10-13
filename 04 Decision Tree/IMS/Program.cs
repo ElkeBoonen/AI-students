966ccb01bb594b2e09d8a1d9eb58eb6ddcb18bcc
Elke Boonen <u0116591@thomasmore.be>
2023-10-13 14:56:31 +0200

    13/10

---
+++ 04 Decision Tree/IMS/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] X = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] Y = { 0, 0, 0, 1, 1, 1, 1, 1, 1 };
+
+            DT tree = new DT();
+            Node root = tree.Train(X, Y, 0, 4);
+
+          
+            tree.Print(root); ;
+        }
+    }
+}
