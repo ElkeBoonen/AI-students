81c1bd76ba9a70458f351216f8145770050206d9
Elke Boonen <u0116591@thomasmore.be>
2023-10-26 14:35:20 +0200

    26/10

---
+++ 06 KNN/DSPS/KNN.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPS
+{
+    class KNN
+    {
+        private Dictionary<double[], string> Data { get; set; }
+
+        public KNN(string file)
+        {
+            Data = new Dictionary<double[], string>();
+            ReadData(file);
+        }
+
+        private void ReadData(string file)
+        {
+            //5,3.6,1.4,0.2,setosa
+            foreach (var item in File.ReadAllLines("iris.dat"))
+            {
+                List<string> strings = item.Split(',').ToList();
+                string value = strings[strings.Count - 1];
+                strings.RemoveAt(strings.Count - 1);
+
+                double[] features = Array.ConvertAll(strings.ToArray(), Convert.ToDouble);
+                Data[features] = value;
+            }
+        }
+
+        private double CalculateDistance(double[] p, double[] q)
+        { }
+
+        public string Predict(double[] array)
+        { 
+        
+        }
+    }
+}
---
+++ 06 KNN/DSPS/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KNN knn = new KNN("iris.dat");
+
+            Console.WriteLine(knn.Classify(
+                    new double[] { 6.1, 2.6, 5.6, 1.4 }));
+        }
+    }
+}
