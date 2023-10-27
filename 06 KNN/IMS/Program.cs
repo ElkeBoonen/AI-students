dabe16f491d42381d4db04b41a042c4502fd26e1
Elke Boonen <u0116591@thomasmore.be>
2023-10-27 15:14:32 +0200

    27/10

---
+++ 06 KNN/IMS/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+            KNN knn = new KNN("iris.dat");
+            Console.WriteLine(knn.Classify(new double[] { 6.1, 2.6, 5.6, 1.4 }));
+            Console.WriteLine(knn.Classify(new double[] { 5.4, 3.4, 1.7, 0.2 }));
+            Console.WriteLine(knn.Classify(new double[] { 6.1, 2.8, 4.7, 1.2 }));
+
+        }
+    }
+}
