8117397e83cc23a8c760c8f5bdcbd6b6f2e2f116
Elke Boonen <u0116591@thomasmore.be>
2023-11-16 14:38:53 +0100

    16/11

---
+++ 07 Matrix Factorization/IMS/MatrixFactorization.cs
@@ -0,0 +1,85 @@
+﻿namespace IMS
+{
+    class MatrixFactorization
+    {
+        int[,] userItemMatrix;
+        int nUsers;
+        int nItems;
+        int nFactors;
+        const double learningRate = 0.01;
+        const int maxIterations = 1000;
+
+        public double[,] userFactors { get; private set; }
+        public double[,] itemFactors { get; private set; }
+
+        public MatrixFactorization(int[,] matrix, int factors)
+        {
+            userItemMatrix = matrix;
+            nFactors = factors;
+            nUsers = userItemMatrix.GetLength(0);
+            nItems = userItemMatrix.GetLength(1);
+            Create();
+        }
+
+        private void Create()
+        {
+            userFactors = new double[nUsers, nFactors];
+            itemFactors = new double[nFactors, nItems];
+
+            Random random = new Random();
+            for (int i = 0; i < nUsers; i++)
+            {
+                for (int j = 0; j < nFactors; j++)
+                {
+                    userFactors[i, j] = random.NextDouble();
+                }
+            }
+
+            for (int i = 0; i < nFactors; i++)
+            {
+                for (int j = 0; j < nItems; j++)
+                {
+                    itemFactors[i, j] = random.NextDouble();
+                }
+            }
+
+            for (int m = 0; m < maxIterations; m++)
+            {
+                for (int i = 0; i < nUsers; i++)
+                {
+                    for (int j = 0; j < nItems; j++)
+                    {
+
+                        if (userItemMatrix[i, j] > 0)
+                        {
+                            double prediction = 0;
+                            for (int k = 0; k < nFactors; k++)
+                            {
+                                prediction += userFactors[i, k] * itemFactors[k, j];
+                            }
+
+                            double error = userItemMatrix[i,j] - prediction;
+
+                            for (int k = 0; k < nFactors; k++)
+                            {
+                                userFactors[i, k] += learningRate * (2 * error * itemFactors[k, j]);
+                                itemFactors[k, j] += learningRate * (2 * error * userFactors[i, k]);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public double Prediction(int user, int item)
+        {
+            double predict = 0;
+            for (int k = 0; k < nFactors; k++)
+            {
+                predict += userFactors[user, k] * itemFactors[k, item];
+            }
+            return predict;
+        }
+    }
+}
---
+++ 08 Naive Bayes/DSPS/NB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace DSPS
+{
+    class NB
+    {
+        public NB(string file)
+        {
+
+        }
+        public double Predict(string sentence) //prediction of spam
+        { }
+
+        public bool IsSpam(string sentence)
+        { }
+    }
+}
