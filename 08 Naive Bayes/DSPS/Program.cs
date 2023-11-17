e1782ee93f7e112453a900a412637be782ea7d6a
Elke Boonen <u0116591@thomasmore.be>
2023-11-17 15:59:06 +0100

    17/11

--- 08 Naive Bayes/DSPS/Program.cs
+++ 08 Naive Bayes/DSPS/Program.cs
@@ -7,6 +7,8 @@
             NB nb = new NB("data.txt");
             Console.WriteLine(nb.Predict("This is a test sentence, is this spam?"));
             Console.WriteLine(nb.IsSpam("This is a test sentence, is this spam?"));
+            Console.WriteLine(nb.IsSpam("Free entry in 2 a wkly comp to win FA Cup final tkts 21st May 2005. Text FA to 87121 to receive entry question(std txt rate)T&C's apply 08452810075over18'"));
+
         }
     }
 }
---
+++ 08 Naive Bayes/IMS/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NB nb = new NB("data.txt");
+            Console.WriteLine(nb.Predict("As a valued network customer you have been selected to receivea"));
+            Console.WriteLine(nb.Predict("Elephants are animals"));
+
+        }
+    }
+}
