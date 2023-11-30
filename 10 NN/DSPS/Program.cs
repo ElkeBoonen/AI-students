ff1c6bf78ce0f5ebde2dd83ea5d09a47af454248
Elke Boonen <u0116591@thomasmore.be>
2023-11-30 15:46:44 +0100

    30/11

--- 10 NN/DSPS/Program.cs
+++ 10 NN/DSPS/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[,] training_input = new double[,] { { 0, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 }, { 0, 1, 1 } };
+            double[,] training_output = Matrix.Transpose(new double[,] { { 0, 1, 1, 0 } });
+
+            var nn = new NN(training_input, training_output);
+
+            Console.WriteLine("Weights before training:");
+            Matrix.Print(nn.Weights);
+
+            nn.Train();
+
+            Console.WriteLine("Weights after training:");
+            Matrix.Print(nn.Weights);
+
+            double output = nn.Predict(new double[,] { { 1, 0, 1 } });
+            Console.WriteLine("Predict [1, 0, 1] => " + output);
+
+            output = nn.Predict(new double[,] { { 0,0,0 } });
+            Console.WriteLine("Predict [0,0,1] => " + output);
         }
     }
 }
