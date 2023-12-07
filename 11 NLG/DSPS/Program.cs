b21188ec90c1d9b19d912d6d5b6f6cc64445791d
Elke Boonen <u0116591@thomasmore.be>
2023-12-07 14:36:09 +0100

    7/12

---
+++ 11 NLG/DSPS/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleTextGeneratorNN textGenerator = new SimpleTextGeneratorNN();
+            textGenerator.Train("The quick brown fox jumps over the lazy dog near the river bank.");
+            string generatedText = GenerateText(textGenerator, startChar: 't', length: 5);
+            Console.WriteLine($"Generated text: {generatedText}");
+        }
+
+        static string GenerateText(SimpleTextGeneratorNN nn, char startChar, int length)
+        {
+            string text = startChar.ToString();
+            char nextChar = startChar;
+
+            for (int i = 0; i < length; i++)
+            {
+                nextChar = nn.PredictNextChar(nextChar);
+                text += nextChar;
+            }
+
+            return text;
+        }
+    }
+}
---
+++ 11 NLG/DSPS/SimpleTextGeneratorNN.cs
@@ -0,0 +1,34 @@
+﻿namespace DSPS
+{
+    public class SimpleTextGeneratorNN
+    {
+        private const int nrOfChars = 26;
+        private const double learningRate = 0.1;
+        private const int epochs = 1000;
+        public double[,] Weights { get; private set; }
+
+        public SimpleTextGeneratorNN()
+        {
+            Random rd = new Random();
+            Weights = new double[nrOfChars, nrOfChars];
+            for (int i = 0; i < nrOfChars; i++)
+            {
+                for (int j = 0; j < nrOfChars; j++)
+                {
+                    Weights[i, j] = rd.NextDouble();
+                }
+            }
+        }
+
+        internal void Train(string v)
+        {
+            throw new NotImplementedException();
+        }
+        internal char PredictNextChar(char nextChar)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
