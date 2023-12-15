3049ec0605d797d04bfbc4e51bb605b152affdb4
Elke Boonen <u0116591@thomasmore.be>
2023-12-15 15:03:48 +0100

    15/12

--- 12 GA/IMS/GeneticAlgorithm.cs
+++ 12 GA/IMS/GeneticAlgorithm.cs
@@ -7,6 +7,7 @@
         public int NumberOfSelections { get; set; }
 
         const int maxGenerations = 50;
+        Random random = new Random();
 
         public GeneticAlgorithm(int populationSize, int chromosoneLength, int numberOfSelections)
         {
@@ -19,14 +20,13 @@
         {
             int generationCount = 0;
 
-
             // Initialization
-            List<string> population = InitializePopulation(populationSize, chromosomeLength);
+            List<string> population = InitializePopulation();
 
             while (generationCount < maxGenerations)
             {
                 // Selection
-                List<string> selected = Selection(population, numberOfSelections);
+                List<string> selected = Selection(population);
 
                 // Crossover and Mutation
                 List<string> newPopulation = new List<string>();
@@ -44,6 +44,52 @@
             // Output the final population
             foreach (var individual in population)
                 Console.WriteLine($"Chromosome: {individual}, Fitness: {CalculateFitness(individual)}");
+        }
+
+        private int CalculateFitness(string individual)
+        {
+            int count = 0;
+            foreach (char c in individual)
+            {
+                if (c == '1') count++;
+            }
+            return count;
+        }
+
+        private List<string> Selection(List<string> population)
+        {
+            for (int i = 0; i < population.Count - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < population.Count; j++)
+                {
+                    if (CalculateFitness(population[j]) < CalculateFitness(population[min])) min = j;
+                }
+                string b = population[i];
+                population[i] = population[min];
+                population[min] = b;
+            }
+            
+
+
+            List<string> list = new List<string>();
+
+        }
+
+        private List<string> InitializePopulation()
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < PopulationSize; i++)
+            {
+                string chromosone = "";
+                for (int j = 0; j < ChromosoneLength; j++)
+                {
+                    int n = random.Next(2);
+                    chromosone += n.ToString();
+                }
+                list.Add(chromosone);
+            }
+            return list;
         }
     }
 }
